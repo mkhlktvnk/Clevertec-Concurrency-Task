b0e5dc68fe3841d2f9e4a240a03bbd0638f1d6e6
misha <mikhail.kutovenko@gmail.com>
2023-02-28 20:50:28 +0300

    Add @AllArgsConstructor

--- src/main/java/ru/clevertec/model/Request.java
+++ src/main/java/ru/clevertec/model/Request.java
@@ -1,9 +1,12 @@
 package ru.clevertec.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class Request {
     private int value;
     private int valueFactor;
 }
+
