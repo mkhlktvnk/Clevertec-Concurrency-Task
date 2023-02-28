72ffb103240e9136a6bc703f87c914706f883a25
misha <mikhail.kutovenko@gmail.com>
2023-02-28 21:11:12 +0300

    Add Server

---
+++ src/main/java/ru/clevertec/model/ru/clevertec/server/Server.java
@@ -0,0 +1,25 @@
+package ru.clevertec.model.ru.clevertec.server;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import lombok.SneakyThrows;
+import ru.clevertec.model.Request;
+import ru.clevertec.model.Response;
+
+import java.security.SecureRandom;
+
+@NoArgsConstructor(staticName = "getInstance")
+public class Server {
+    private final SecureRandom secureRandom = new SecureRandom();
+    private static final long MAX_DELAY_MS = 2000;
+
+    @SneakyThrows
+    public Response handleRequest(Request request) {
+        Thread.sleep(generateDelay());
+        return new Response(request.getValue() * request.getValueFactor());
+    }
+
+    private long generateDelay() {
+        return secureRandom.nextLong(MAX_DELAY_MS + 1);
+    }
+}
